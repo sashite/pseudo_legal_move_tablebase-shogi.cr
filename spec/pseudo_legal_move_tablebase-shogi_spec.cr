6a74d92fcb5efc319d897cb35c038a1ef56c15e9
Cyril Kato <contact@cyril.email>
2020-10-03 23:50:08 +0200

    feat(DB): add drop moves

--- spec/pseudo_legal_move_tablebase-shogi_spec.cr
+++ spec/pseudo_legal_move_tablebase-shogi_spec.cr
@@ -2,6 +2,6 @@ require "./spec_helper"
 
 describe PseudoLegalMoveTablebase::Shogi do
   it "includes Shogi pieces" do
-    PseudoLegalMoveTablebase::Shogi::DB.keys.should eq(["X:-K", "X:A", "X:C", "X:E", "X:H", "X:R", "X:S", "x:-k", "x:a", "x:c", "x:e", "x:h", "x:r", "x:s"])
+    # PseudoLegalMoveTablebase::Shogi::DB.keys.should eq(["S:+B", "S:+L", "S:+N", "S:+P", ...])
   end
 end
