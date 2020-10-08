3bc2e5683aad7f3ec1ef0b3da719ba4112e222f1
Cyril Kato <contact@cyril.email>
2020-10-08 11:18:24 +0200

    test(DB): add keys verification

--- spec/pseudo_legal_move_tablebase-shogi_spec.cr
+++ spec/pseudo_legal_move_tablebase-shogi_spec.cr
@@ -2,6 +2,37 @@ require "./spec_helper"
 
 describe PseudoLegalMoveTablebase::Shogi do
   it "includes Shogi pieces" do
-    # PseudoLegalMoveTablebase::Shogi::DB.keys.should eq(["S:+B", "S:+L", "S:+N", "S:+P", ...])
+    PseudoLegalMoveTablebase::Shogi::DB.keys.sort.should eq(
+      %w(
+        S:+B
+        S:+L
+        S:+N
+        S:+P
+        S:+R
+        S:+S
+        S:-K
+        S:B
+        S:G
+        S:L
+        S:N
+        S:P
+        S:R
+        S:S
+        s:+b
+        s:+l
+        s:+n
+        s:+p
+        s:+r
+        s:+s
+        s:-k
+        s:b
+        s:g
+        s:l
+        s:n
+        s:p
+        s:r
+        s:s
+      ).sort
+    )
   end
 end
